d346ada2f6b2a331f420741217b6d958e3fc04b3
chepur <chepurev@gmail.com>
2016-04-14 14:46:35 +0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
   resources :users
-  get 'signup', to: 'users#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signup',  to: 'users#new',         via: 'get'
+  match '/signin',  to: 'sessions#new',      via: 'get'
+  match '/signout', to: 'sessions#destroy',  via: 'delete'
 
   root 'static_pages#home'
 
-  get 'help', to: 'static_pages#help'
-  get 'about', to: 'static_pages#about'
+  get 'help',    to: 'static_pages#help'
+  get 'about',   to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
